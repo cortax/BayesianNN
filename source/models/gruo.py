89692315462cb966754908ecbe581e71a15f4d8b
Maxime Lapointe <maxime.lapointe.4@ulaval.ca>
2019-09-17 18:14:09 -0400

    Code Base upgrade

--- source/models/gruo.py
+++ source/models/gruo.py
@@ -2,28 +2,30 @@ import torch
 import torch.nn as nn
 #from torch.nn.utils.rnn import pack_padded_sequence
 
-class GatedRecurrentUnitHnNet(nn.Module):
+
+class GatedRecurrentUnitOutputNet(nn.Module):
     def __init__(self, input_size, output_size, 
                         window_size, hidden_size, num_layers, 
                         target_type_string='Regression',
                         bias=True, batch_first=True, 
                         bidirectional=False,
-                        dropout_hidden=0, dropout_Hn=0):
+                        dropout_hidden=0, dropout_output=0):
         super().__init__()
         self.bidirectional = bidirectional
         self.seq_len = window_size
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         
-        self.gru = nn.GRU(input_size=input_size,
-                            hidden_size=hidden_size,
-                            num_layers=num_layers,
-                            bias=bias,
-                            batch_first=batch_first,
+        self.gru = nn.GRU(input_size=input_size, 
+                            hidden_size=hidden_size, 
+                            num_layers=num_layers, 
+                            bias=bias, 
+                            batch_first=batch_first, 
                             dropout=dropout_hidden, 
                             bidirectional=bidirectional)
-        self.hn_dropout_layer = nn.Dropout(dropout_Hn)
-        self.predict_layer = nn.Linear(hidden_size * num_layers * (bidirectional + 1), output_size) #(hidden*layers*1, 1)
+        self.output_dropout_layer = nn.Dropout(dropout_output)
+
+        self.predict_layer = nn.Linear(hidden_size * window_size * (bidirectional + 1), output_size) #(seq_len * num_directions * hidden_size)
         
         self.target_type_string = target_type_string
         if target_type_string=='Regression':
@@ -36,17 +38,14 @@ class GatedRecurrentUnitHnNet(nn.Module):
         #print(input.size())
         #print(input[0])
         #print(input[0][0])
-        gru_out, h_n = self.gru(input) #h_n shape: (num_layers * num_directions, batch, hidden_size)
-        #print('h_n:', h_n)
-        #print('h_n size:', h_n.size(0), h_n.size(1), h_n.size(2))
-        h_n_transpose = torch.transpose(h_n, 0, 1).contiguous() #shape: (batch, num_layers * num_directions, hidden_size)
-        #print('h_n_transpose:', h_n_transpose)
-        #print('h_n_transpose size:', h_n_transpose.size(0), h_n_transpose.size(1), h_n_transpose.size(2))
-        h_n_view = h_n_transpose.view(input.size(0), -1) #shape: (batch, num_layers * num_directions * hidden_size)
+        gru_out, h_n = self.gru(input) #shape gru_out: (batch, seq_len, num_directions * hidden_size)
+        #print('gru_out:', gru_out)
+        #print('gru_out size:', gru_out.size(0), gru_out.size(1), gru_out.size(2))
+        h_n_view = gru_out.contiguous().view(input.size(0), -1) #shape: (batch, seq_len*num_directions * hidden_size)
         #print('h_n_view:', h_n_view)
         #print('h_n_view size:', h_n_view.size(0), h_n_view.size(1))
-        h_n_dropout = self.hn_dropout_layer(h_n_view)
-        predict = self.predict_layer(h_n_dropout) #predict est vertical
+        out_dropout = self.output_dropout_layer(h_n_view)
+        predict = self.predict_layer(out_dropout) #predict est vertical
         #print('predict:', predict)
         #print('predict size:', predict.size())
         return predict
