10db6b455045282595ceef1e9ce5c2a10e9f37f7
Mathieu Alain <mathieu.alain.2@ulaval.ca>
2019-10-09 19:48:59 -0400

    Modification de evaluation_phase

--- Experiments/foong/tanh/MAP/evaluation_phase.py
+++ Experiments/foong/tanh/MAP/evaluation_phase.py
@@ -6,23 +6,41 @@ rootdir = dirname(dirname(dirname(dirname(cwd))))
 sys.path.append( rootdir )
 
 
+
 from Inference import BBVI 
 import _pickle as pickle
 import torch
 import matplotlib.pyplot as plt
 
-def plot_validation(model, model_name):
+def plot(model, model_name):
+        
+    y_train = model.forward(x_data)    
         
     y_validation = model.forward(x_data_validation)
 
-    ELL = model._log_norm(y_validation, y_data_validation, torch.tensor(0.1).to(device))
-    ELL = ELL.sum(dim=[1,2]).mean().detach().cpu().numpy()
+    y_test = model.forward(x_data_test)
+    
+    ELL_train = model._log_norm(y_train, y_data, torch.tensor(0.1).to(device))
+    ELL_train = ELL_train.sum(dim=[1,2]).mean().detach().cpu().numpy()
 
-    ELBO = model.compute_elbo(x_data, y_data, n_samples_ELBO=1000, sigma_noise=0.1, device=device).detach().cpu().numpy()
+        
+    ELL_val = model._log_norm(y_validation, y_data_validation, torch.tensor(0.1).to(device))
+    ELL_val = ELL_val.sum(dim=[1,2]).mean().detach().cpu().numpy()
+    
+    ELL_test = model._log_norm(y_test, y_data_test, torch.tensor(0.1).to(device))
+    ELL_test = ELL_test.sum(dim=[1,2]).mean().detach().cpu().numpy()
 
+    ELBO_train = model.compute_elbo(x_data, y_data, n_samples_ELBO=1000, sigma_noise=0.1, device=device).detach().cpu().numpy()
+    ELBO_val = model.compute_elbo(x_data_validation, y_data_validation, n_samples_ELBO=1000, sigma_noise=0.1, device=device).detach().cpu().numpy()
+    ELBO_test = model.compute_elbo(x_data_test, y_data_test, n_samples_ELBO=1000, sigma_noise=0.1, device=device).detach().cpu().numpy()
+    
+    INFOS = [ELL_train, ELL_val, ELL_test, ELBO_train, ELBO_val, ELBO_test]
+    
     y_real = torch.cos(4.0 * (x_linspace + 0.2))
     
-    plt.axis([-2, 2, -2, 3])
+    fig = plt.figure() 
+#    plt.title(model_name)
+    plt.axis([-2, 2, -2, 3.5])
     plt.scatter(x_data.cpu(), y_data.cpu(), s=3)
     plt.plot(x_linspace.cpu(), y_real.cpu())
 
@@ -30,29 +48,37 @@ def plot_validation(model, model_name):
 
         model.sample_parameters()
 
-        y_validation_PD = model.forward(x_linspace)
+        y_test_PD = model.forward(x_linspace)
         
-        plt.plot(x_linspace.detach().cpu().numpy(), y_validation_PD.squeeze(0).detach().cpu().numpy(), alpha=0.05, linewidth=1, color='lightblue')
+        plt.plot(x_linspace.detach().cpu().numpy(), y_test_PD.squeeze(0).detach().cpu().numpy(), alpha=0.05, linewidth=1, color='lightblue')
         
-    plt.text(-1.7, 2, 'Expected log-likelihood (val): '+str(ELL), fontsize=11)
-    plt.text(-1.7, 2.5, 'ELBO (train): '+ str(ELBO), fontsize=11)
-
+    plt.text(-1.7, 3, 'Expected log-likelihood (train): '+str(ELL_train), fontsize=10)
+    plt.text(-1.7, 2.75, 'Expected log-likelihood (val): '+str(ELL_val), fontsize=10)
+    plt.text(-1.7, 2.5, 'Expected log-likelihood (test): '+str(ELL_test), fontsize=10)
+    plt.text(-1.7, 2.25, 'ELBO (train): '+ str(ELBO_train), fontsize=10)
+    plt.text(-1.7, 2, 'ELBO (val): '+ str(ELBO_val), fontsize=10)
+    plt.text(-1.7, 1.75, 'ELBO (test): '+ str(ELBO_test), fontsize=10)
+   
     plt.savefig(cwd + '/plots/' + model_name)
 
-    return ELL, ELBO
+    return INFOS
     
     
-def update_log(model, model_name, ell, elbo):
+def update_log(model, model_name, INFOS):
     
     log = open(cwd + '/logs/' + model_name + '.txt', 'a+')
 
-    log.write('Expected log-likelihood on the validation set:' + str(ell) + '\n')
-    log.write('ELBO on the train set:' + str(elbo) + '\n')
+    log.write('Expected log-likelihood on the train set:' + str(INFOS[0]) + '\n')
+    log.write('Expected log-likelihood on the validation set:' + str(INFOS[1]) + '\n')
+    log.write('Expected log-likelihood on the test set:' + str(INFOS[2]) + '\n')
+
+    log.write('ELBO on the train set:' + str(INFOS[3]) + '\n')
+    log.write('ELBO on the train set:' + str(INFOS[4]) + '\n')
+    log.write('ELBO on the train set:' + str(INFOS[5]) + '\n')
     
     log.close()
   
-        
-        
+
         
 if __name__ == "__main__":
     
@@ -60,14 +86,13 @@ if __name__ == "__main__":
 
     activation = torch.tanh
 
-    layer_size = [2]
-    nb_layer = [2]
-    nb_trial = 1
+    layer_size = [10,25,50,100]
+    nb_layer = [2,3,4]
+    nb_trial = 30
     
-#    layer_size = [10,25,50,100]
-#    nb_layer = [2,3,4]
-#    nb_trial = 10
-    
+#    layer_size = [100]
+#    nb_layer = [2]
+
     data = torch.load(rootdir + '/Data/foong_data.pt')
     x_data = data[0].to(device)
     y_data = data[1].to(device)
@@ -77,19 +102,28 @@ if __name__ == "__main__":
     x_data_validation = data[0].to(device)
     y_data_validation = data[1].to(device)
     y_data_validation = y_data_validation.unsqueeze(-1)
+    
+    data = torch.load(rootdir +'/Data/foong_data_test.pt')
+    x_data_test = data[0].to(device)
+    y_data_test = data[1].to(device)
+    y_data_test = y_data_test.unsqueeze(-1)
 
     x_linspace = torch.linspace(-2.0, 2.0).unsqueeze(1).to(device)
 
     for L in nb_layer:
         for W in layer_size:
+#           for j in [20]:
             for j in range(nb_trial):
                 Net_name = str(L) + 'Layers_' + str(W) + 'Neurons_(' + str(j) +')'
-                filename = cwd+ '/models/' + Net_name
+                filename = cwd + '/models/' + Net_name
                 filehandler = open(filename, 'rb') 
                 Net = pickle.load(filehandler)
+                
+#                Net.to(device)
                 filehandler.close()
                 
-                ELL, ELBO = plot_validation(Net, Net_name)
-                update_log(Net, Net_name, ELL, ELBO)
+                INFOS = plot(Net, Net_name)
+                update_log(Net, Net_name, INFOS)
 
                 
+
