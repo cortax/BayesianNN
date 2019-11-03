74c9d35dbbc5fc7ca0d2c51c5206885a45b04f6a
Patrick Dallaire <patrickd@gra956.graham.sharcnet>
2019-11-03 12:48:30 -0500

    corrected learning job

--- Experiments/foong/tanh/MAP+BBVI/learning_job.py
+++ Experiments/foong/tanh/MAP+BBVI/learning_job.py
@@ -36,7 +36,7 @@ def train_model(layer_width, nb_layers, activation, seed, Net):
     optimizer = torch.optim.Adam
     optimizer_params = {'lr': 0.1}
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau
-    scheduler_params = {'patience': 2, 'factor': 0.8, 'threshold': 1e-3, 'threshold_mode', 'abs'}
+    scheduler_params = {'patience': 2, 'factor': 0.8, 'threshold': 1e-3, 'threshold_mode': 'abs'}
 
     voptimizer = BBVI.VariationalOptimizer(model=Net, sigma_noise=0.1, optimizer=optimizer, optimizer_params=optimizer_params, scheduler=scheduler, scheduler_params=scheduler_params, min_lr=0.00001)
     Net = voptimizer.run((x_data,y_data), n_epoch=100000, n_iter=150, seed=seed, n_ELBO_samples=75, verbose=1)
@@ -81,6 +81,13 @@ if __name__ == "__main__":
         start_time = time.time() 
         Net, training_infos = train_model(W, L, activation, j, Net)
         training_time = time.time() - start_time 
+
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        data = torch.load(rootdir + 'Data/foong_data.pt')
+        x_data = data[0].to(device)
+        y_data = data[1].to(device)
+        y_data = y_data.unsqueeze(-1)
+
         
         print(Net.compute_elbo(x_data, y_data, n_samples_ELBO=1000, sigma_noise=0.1, device=device).detach().cpu().numpy())
 
