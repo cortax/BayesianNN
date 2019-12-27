3be84b8b560c85b166154042220e8ab6cca85b33
yannpequignot <56802873+yannpequignot@users.noreply.github.com>
2019-12-27 16:14:40 -0500

    Update BBVAE_on_NN avec explications théoriques, nouveau notebook test (2D,2D-2map),add VariationalAE2

---
+++ Inference/VariationalAE2.py
@@ -0,0 +1,38 @@
+from torch import nn
+import torch
+from livelossplot import PlotLosses
+from Inference.Variational import MeanFieldVariationalDistribution
+
+class MeanFieldVariationalAE(nn.Module):
+    def __init__(self, lat_dim, H,H_, weights_dim, extraLayer=False, activation=nn.Sigmoid(), mu=0.0, sigma=1.0, device='cpu'):
+        super(MeanFieldVariationalAE, self).__init__()
+        self.device = device
+        self.lat_dim = lat_dim
+        self.weights_dim = weights_dim
+        self.mfvar = MeanFieldVariationalDistribution(lat_dim,mu,sigma)
+        self.layerIn=nn.Linear(lat_dim, H)
+        self.layerOut=nn.Linear(H_,weights_dim)
+        if extraLayer: 
+            self.layerMid=nn.Linear(H, H_)
+            self.decoder=nn.Sequential(
+                       self.layerIn,
+                       activation,
+                       self.layerMid,
+                       nn.ReLU(),
+                       self.layerOut
+                       )
+        else:
+            self.decoder=nn.Sequential(
+               self.layerIn,
+               activation,
+               self.layerOut
+               )
+          
+    def forward(self, n=1):
+        sigma = self.mfvar.sigma
+        epsilon = torch.randn(size=(n,self.lat_dim)).to(self.device)
+        lat=epsilon.mul(sigma).add(self.mfvar.mu)
+        return self.decoder(lat)
+
+
+    
