4d55519861eb6f510ec38b259ff538e99d649b15
Patrick Dallaire <patrick.dallaire@ift.ulaval.ca>
2019-12-29 15:13:16 -0500

    correction

--- runxp_MAP_normal.sh
+++ runxp_MAP_normal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --array=0-100%100
+#SBATCH --array=0-2%100
 #SBATCH --time=12:00:00
 #SBATCH --account=rrg-corbeilj-ac
 #SBATCH --gres=gpu:0              # Number of GPUs (per node)
