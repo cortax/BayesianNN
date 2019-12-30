c45c5b7f23c464c8d286d274c5e5dd54d8e0946c
Patrick Dallaire <patrick.dallaire@ift.ulaval.ca>
2019-12-30 03:39:13 -0500

    new XPs

--- runxp_PTMCMC_hot.sh
+++ runxp_PTMCMC_hot.sh
@@ -4,7 +4,7 @@
 #SBATCH --account=rrg-corbeilj-ac
 #SBATCH --gres=gpu:0              # Number of GPUs (per node)
 #SBATCH --mem=32G               # memory (per node)
-python PTMCMC_hot.py --layerwidth 50 --nblayers 2 --tag $SLURM_ARRAY_TASK_ID --nbchunk 10 --chunksize 100000 --device cpu 
+python PTMCMC_hot.py --layerwidth 50 --nblayers 1 --tag $SLURM_ARRAY_TASK_ID --nbchunk 10 --chunksize 100000 --device cpu 
 
 
 
