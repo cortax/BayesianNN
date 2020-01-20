26e8e087e5cb3cf1c7a166108544027f84858dd6
Patrick Dallaire <patrick.dallaire@ift.ulaval.ca>
2020-01-19 20:20:46 -0500

    job script

---
+++ runxp_MAP.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --array=0-2%500
+#SBATCH --time=4:00:00
+#SBATCH --account=rrg-corbeilj-ac
+#SBATCH --gres=gpu:0              # Number of GPUs (per node)
+#SBATCH --mem=8G               # memory (per node)
+python -m Experiments.Foong_L1W50.MAP --max_iter=100000 max_iter=100000, learning_rate=0.05, min_lr=0.0005, patience=200, lr_decay=0.9, gamma_alpha=1.0, gamma_beta=1.0
+
+
+
+
