4c6b4f1815ff5e246ff78b9087abf05d4ed6ee90
Patrick Dallaire <patrickd@gra-login1.graham.sharcnet>
2020-01-19 21:33:48 -0500

    working batch script for MAP

--- runxp_MAP.sh
+++ runxp_MAP.sh
@@ -4,7 +4,7 @@
 #SBATCH --account=rrg-corbeilj-ac
 #SBATCH --gres=gpu:0              # Number of GPUs (per node)
 #SBATCH --mem=8G               # memory (per node)
-python -m Experiments.Foong_L1W50.MAP --max_iter=100000 max_iter=100000, learning_rate=0.05, min_lr=0.0005, patience=200, lr_decay=0.9, gamma_alpha=1.0, gamma_beta=1.0
+python -m Experiments.Foong_L1W50.MAP --max_iter=100000 --max_iter=100000 --learning_rate=0.05 --min_lr=0.0005 --patience=200 --lr_decay=0.9 --gamma_alpha=1.0 --gamma_beta=1.0
 
 
 
