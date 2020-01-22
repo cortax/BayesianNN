d194ccbe2e33e82bd54b5279567371285eeb0ffb
yannpequignot <56802873+yannpequignot@users.noreply.github.com>
2020-01-21 23:16:02 -0500

    Update VaHyNet-KDE.py

--- Experiments/Foong_L1W50/VaHyNet-KDE.py
+++ Experiments/Foong_L1W50/VaHyNet-KDE.py
@@ -174,7 +174,7 @@ def main(ensemble_size=1,lat_dim=5,activation=nn.ReLU(),init_w=.15,init_b=.001,K
             plt.scatter(x_train.cpu(), y_train.cpu())
             theta = Hyper_Nets(1000)
             for i in range(1000):
-                y_test = model(theta[c,i].unsqueeze(0),x_test)
+                y_test = model(theta[i].unsqueeze(0),x_test)
                 plt.plot(x_test.detach().cpu().numpy(), y_test.squeeze(0).cpu().numpy(), alpha=0.05, linewidth=1, color='green')
                 #    plt.plot(x_test.cpu(), y_test.squeeze(0).detach().cpu().numpy(), alpha=0.05, linewidth=1, color='C'+str(c))
             fig.savefig(tempdir.name+'/training.png', dpi=4*fig.dpi)
