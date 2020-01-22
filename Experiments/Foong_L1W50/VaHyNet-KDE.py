bb2475c07ec44b38c130f35b17749445a5e330ea
yannpequignot <56802873+yannpequignot@users.noreply.github.com>
2020-01-22 00:11:22 -0500

    Update VaHyNet-KDE.py

--- Experiments/Foong_L1W50/VaHyNet-KDE.py
+++ Experiments/Foong_L1W50/VaHyNet-KDE.py
@@ -209,7 +209,7 @@ def main(ensemble_size=1,lat_dim=5,activation=nn.ReLU(),init_w=.15,init_b=.001,K
             plt.scatter(x_test.cpu(), y_test.cpu())
             plt.rcParams['agg.path.chunksize'] = 1000
             theta = Hyper_Nets(1000)
-            for i in range(1000)
+            for i in range(1000):
                 y_test = model(theta[i].unsqueeze(0),x_test)
                 plt.plot(x_test.cpu(), y_test.squeeze(0).detach().cpu().numpy(), alpha=0.05, linewidth=1, color='green')
             fig.savefig(tempdir.name+'/test.png', dpi=4*fig.dpi)
