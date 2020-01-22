68f947a6c3bf698521dead246af80314e28b815e
yannpequignot <56802873+yannpequignot@users.noreply.github.com>
2020-01-22 11:08:56 -0500

    Update VaHyNet-KDE.py

--- Experiments/Foong_L1W50/VaHyNet-KDE.py
+++ Experiments/Foong_L1W50/VaHyNet-KDE.py
@@ -41,11 +41,11 @@ class HyNetEns(nn.Module):
 
     # "Silverman's rule of thumb", Wand and Jones p.111 "Kernel Smoothing" 1995.                                 
     def get_H(self, nb_samples):
-        theta=self.forward(nb_samples).to(device)
+        theta=self.forward(nb_samples)
         c=torch.tensor(((nb_samples*(self.output_dim+2))/4)).pow(2/(self.output_dim+4)).to(device)     
         H_=theta.var(0)/c
         #H_=theta.var(0).min(1).values/c*torch.ones(self.output_dim) #to try!
-        return theta, H_.clamp(torch.finfo().eps.to(device),float('inf').to(device))
+        return theta, H_.clamp(torch.finfo().eps,float('inf'))
 
     def KDE(self, theta_,theta, H):
         def kernel(theta1,theta2):
