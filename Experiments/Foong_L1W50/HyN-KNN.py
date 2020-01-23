30034b6d2dc7fb02b244be4026b5e7610ac4cfa1
yannpequignot <56802873+yannpequignot@users.noreply.github.com>
2020-01-23 18:09:51 -0500

    Update HyN-KNN.py

--- Experiments/Foong_L1W50/HyN-KNN.py
+++ Experiments/Foong_L1W50/HyN-KNN.py
@@ -140,13 +140,13 @@ def main(ensemble_size=1,lat_dim=5,init_w=.2,init_b=.001,n_samples_KNN=1000,n_sa
             mlflow.log_artifact(tempdir.name+'/training_loss.csv')
             nb_samples_postpred=int(np.ceil(10000/ensemble_size))
             logposteriorpredictive = exp.get_logposteriorpredictive_parallel_fn('cpu')
-            train_post = logposteriorpredictive(Hyper_Nets(nb_samples_postpred).cpu(), model, x_train.cpu(), y_train.cpu(), 0.1)/len(y_train.cpu())
+            train_post = logposteriorpredictive(Hyper_Nets(nb_samples_postpred).cpu(), model, x_train.cpu(), y_train.cpu(), 0.1)
             mlflow.log_metric("training log posterior predictive", -float(train_post.detach().cpu()))
-            val_post = logposteriorpredictive(Hyper_Nets(nb_samples_postpred).detach().cpu(), model, x_validation.cpu(), y_validation.cpu(), 0.1)/len(y_validation.cpu())
+            val_post = logposteriorpredictive(Hyper_Nets(nb_samples_postpred).detach().cpu(), model, x_validation.cpu(), y_validation.cpu(), 0.1)
             mlflow.log_metric("validation log posterior predictive", -float(val_post.detach().cpu()))
-            test_post = logposteriorpredictive(Hyper_Nets(nb_samples_postpred).detach().cpu(), model, x_test.cpu(), y_test.cpu(), 0.1)/len(y_test.cpu())
+            test_post = logposteriorpredictive(Hyper_Nets(nb_samples_postpred).detach().cpu(), model, x_test.cpu(), y_test.cpu(), 0.1)
             mlflow.log_metric("test log posterior predictive", -float(test_post.detach().cpu()))
-            test_ib_post = logposteriorpredictive(Hyper_Nets(nb_samples_postpred).detach().cpu(), model, x_test_ib.cpu(), y_test_ib.cpu(), 0.1)/len(y_test_ib.cpu())
+            test_ib_post = logposteriorpredictive(Hyper_Nets(nb_samples_postpred).detach().cpu(), model, x_test_ib.cpu(), y_test_ib.cpu(), 0.1)
             mlflow.log_metric("test in between log posterior predictive", -float(test_ib_post.detach().cpu()))
 
             
