de8352610cb323a97fe3af5ae93003c211e317e2
Patrick Dallaire <patrick.dallaire@ift.ulaval.ca>
2020-02-08 14:41:39 -0500

    small improvements

--- Experiments/boston/map.py
+++ Experiments/boston/map.py
@@ -5,25 +5,19 @@ import mlflow.pytorch
 import tempfile
 import argparse
 
-
-from Inference.map_method import main, parser
-
+from Inference.map_method import main, getParser
 import Experiments.boston.setup as data
 
-
-
-    
-
 if __name__== "__main__":
+    parser = getParser()
     args = parser.parse_args()
-
     print(args)
 
-    
-if args.device is None:
-    device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
-else:
-    device = args.device   
+    if args.device is None:
+        device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+    else:
+        device = args.device
 
-main(data.get_data,data.get_model,data.sigma_noise,data.experiment_name, data.nb_split,args.ensemble_size,args.init_std, args.max_iter, args.learning_rate, args.min_lr, args.patience, args.lr_decay, device, args.verbose)
+    main(data.get_data, data.get_model, data.sigma_noise, data.experiment_name, data.nb_split, args.ensemble_size,
+        args.init_std, args.max_iter, args.learning_rate, args.min_lr, args.patience, args.lr_decay, device, args.verbose)
 
--- Inference/map_method.py
+++ Inference/map_method.py
@@ -6,15 +6,39 @@ import argparse
 import mlflow
 import mlflow.pytorch
 
-from Prediction.metrics import get_logposterior,log_metrics,log_split_metrics
+from Prediction.metrics import get_logposterior, log_metrics, log_split_metrics
 
-def main(get_data,get_model,sigma_noise,experiment_name,nb_split,ensemble_size,init_std, max_iter, learning_rate, min_lr, patience, lr_decay,  device, verbose):
-    
-    xpname = experiment_name+'/MAP'
+# 1) Ajouter l'EarlyStopping (validation) ou le rollback best (training). En RAM .to('cpu')
+# 2) Vérifier la performance de logger les métriques avec et sans log interne dans la boucle
+
+def getParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ensemble_size", type=int, default=1,
+                        help="number of MAPs to train in the ensemble")
+    parser.add_argument("--init_std", type=float, default=1.0,
+                        help="parameter controling initialization of theta")
+    parser.add_argument("--max_iter", type=int, default=100000,
+                        help="maximum number of learning iterations")
+    parser.add_argument("--learning_rate", type=float, default=0.03,
+                        help="initial learning rate of the optimizer")
+    parser.add_argument("--min_lr", type=float, default=0.00000001,
+                        help="minimum learning rate triggering the end of the optimization")
+    parser.add_argument("--patience", type=int, default=10,
+                        help="scheduler patience")
+    parser.add_argument("--lr_decay", type=float, default=.1,
+                        help="scheduler multiplicative factor decreasing learning rate when patience reached")
+    parser.add_argument("--device", type=str, default=None,
+                        help="force device to be used")
+    parser.add_argument("--verbose", type=bool, default=False,
+                        help="force device to be used")
+    return parser
+
+def main(get_data, get_model, sigma_noise, experiment_name, nb_split, ensemble_size, init_std, max_iter, learning_rate, min_lr, patience, lr_decay,  device, verbose):
+    xpname = experiment_name + '/MAP'
     mlflow.set_experiment(xpname)
     expdata = mlflow.get_experiment_by_name(xpname)
     
-    with mlflow.start_run():#run_name='GeNVI-KDE', experiment_id=expdata.experiment_id
+    with mlflow.start_run(): #run_name='GeNVI-KDE', experiment_id=expdata.experiment_id
         mlflow.set_tag('device', device)
         
         param_count, mlp=get_model()
@@ -38,14 +62,14 @@ def main(get_data,get_model,sigma_noise,experiment_name,nb_split,ensemble_size,i
         for split in range(nb_split):
             with mlflow.start_run(run_name='split', nested=True):
                 X_train, y_train, y_train_un, X_test, y_test_un, inverse_scaler_y = get_data(split, device)
-                logtarget=get_logposterior(mlp,X_train,y_train,sigma_noise,device)
+                logtarget=get_logposterior(mlp, X_train, y_train, sigma_noise, device)
 
-                theta_ens = torch.empty((ensemble_size,param_count),device=device)
+                theta_ens = torch.empty((ensemble_size,param_count), device=device)
 
                 for k in range(ensemble_size):
                     with mlflow.start_run(run_name='component', nested=True):
                         theta = torch.empty((1,param_count), device=device)
-                        torch.nn.init.normal_(theta, 0.,std=init_std)
+                        torch.nn.init.normal_(theta, 0., =init_std)
                         theta.requires_grad=True
 
                         optimizer = torch.optim.Adam([theta], lr=learning_rate)
@@ -59,52 +83,29 @@ def main(get_data,get_model,sigma_noise,experiment_name,nb_split,ensemble_size,i
 
                             lr = optimizer.param_groups[0]['lr']
 
-                            mlflow.log_metric("-log posterior", float(L.detach().clone().cpu().numpy()),t)
-                            mlflow.log_metric("learning rate", float(lr),t)
+                            # TODO: Vérifier la performance ici avec et sans log interne dans la boucle
+                            mlflow.log_metric("-log posterior", float(L.detach().clone().cpu().numpy()), t)
+                            mlflow.log_metric("learning rate", float(lr), t)
                             mlflow.log_metric("epoch", t)
 
-
-                            scheduler.step(L.detach().clone().cpu().numpy())
-
-                            if lr < min_lr:
-                                break
                             if verbose:
                                 stats = 'Split {}.Run {}. Epoch [{}/{}], Training Loss: {}, Learning Rate: {}'.format(split,k,t, max_iter, L, lr)
                                 print(stats)
 
+                            scheduler.step(L.detach().clone().cpu().numpy())
                             optimizer.step()
 
+                            if lr < min_lr:
+                                break
+
                         with torch.no_grad():
-                            log_metrics(theta.detach(), mlp, X_train, y_train_un, X_test, y_test_un, sigma_noise, inverse_scaler_y, t,device)
+                            log_metrics(theta.detach(), mlp, X_train, y_train_un, X_test, y_test_un, sigma_noise, inverse_scaler_y, t, device)
                             theta_ens[k]=theta.squeeze().detach()
-
-
-
 
                 with torch.no_grad():
                     theta=theta_ens
-
-                    splitting_metrics.append(log_metrics(theta, mlp, X_train, y_train_un, X_test, y_test_un, sigma_noise, inverse_scaler_y, t,device))
+                    splitting_metrics.append(log_metrics(theta, mlp, X_train, y_train_un, X_test, y_test_un, sigma_noise, inverse_scaler_y, t, device))
 
         log_split_metrics(splitting_metrics)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--ensemble_size", type=int, default=1,
-                    help="number of MAPs to train in the ensemble")
-parser.add_argument("--init_std", type=float, default=1.0,
-                    help="parameter controling initialization of theta")
-parser.add_argument("--max_iter", type=int, default=100000,
-                    help="maximum number of learning iterations")
-parser.add_argument("--learning_rate", type=float, default=0.03,
-                    help="initial learning rate of the optimizer")
-parser.add_argument("--min_lr", type=float, default=0.00000001,
-                    help="minimum learning rate triggering the end of the optimization")
-parser.add_argument("--patience", type=int, default=10,
-                    help="scheduler patience")
-parser.add_argument("--lr_decay", type=float, default=.1,
-                    help="scheduler multiplicative factor decreasing learning rate when patience reached")
-parser.add_argument("--device", type=str, default=None,
-                    help="force device to be used")
-parser.add_argument("--verbose", type=bool, default=False,
-                    help="force device to be used")
-args = parser.parse_args()
+
