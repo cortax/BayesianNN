0abaf2f058f43e36870679472f199b0b08f6dec3
Yann Pequignot <yann.pequignot@gmail.com>
2020-02-10 00:44:34 -0500

    refactor GeNVI

--- Inference/GeNVI_method.py
+++ Inference/GeNVI_method.py
@@ -6,13 +6,12 @@ import argparse
 import mlflow
 import mlflow.pytorch
 
-from Prediction.metrics import get_logposterior,log_metrics,log_split_metrics
 
 
 
-class HNet(nn.Module):
+class GeNet(nn.Module):
             def __init__(self, lat_dim, nb_neur, output_dim,  activation=nn.ReLU(), init_w=.1, init_b=.1, device='cpu'):
-                super(HNet, self).__init__()
+                super(GeNet, self).__init__()
                 self.lat_dim = lat_dim
                 self.device=device
                 self.output_dim=output_dim
@@ -29,12 +28,12 @@ class HNet(nn.Module):
                 epsilon = torch.randn(size=(n,self.lat_dim)).to(self.device)
                 return self.hnet(epsilon)           
 
-class HyNetEns(nn.Module):
-    def __init__(self,nb_comp,lat_dim,layer_width, output_dim, activation, init_w,init_b,device='cpu'):
-        super(HyNetEns, self).__init__()
+class GeNetEns(nn.Module):
+    def __init__(self, nb_comp, lat_dim, layer_width, output_dim, activation, init_w, init_b, device):
+        super(GeNetEns, self).__init__()
         self.nb_comp=nb_comp
         self.output_dim=output_dim
-        self.components= nn.ModuleList([HNet(lat_dim,layer_width,output_dim,activation,init_w,init_b,device) for i in range(nb_comp)]).to(device)
+        self.components= nn.ModuleList([GeNet(lat_dim,layer_width,output_dim,activation,init_w,init_b,device) for i in range(nb_comp)]).to(device)
 
         self._best_compnents = None
         self._best_score = float('inf')
@@ -80,14 +79,13 @@ Hyper_Nets.sample(100)
 ### Entropy approximation
 
 def get_KDE(device):
-    def KDE(x,y,prec):
+    def KDE(x, x_kde):
         """
         KDE    
 
         Parameters:
             x (Tensor): Inputs, NbExemples X NbDimensions   
-            y (Tensor):  Batched samples, NbBatch x NbSamples X NbDimensions
-            prec (Float): scalar factor for bandwidth scaling
+            x_kde (Tensor):  Batched samples, NbBatch x NbSamples X NbDimensions
 
 
         Returns:
@@ -98,13 +96,13 @@ def get_KDE(device):
 
         dim=x.shape[-1]
         n_ed=x.shape[0]
-        n_comp=y.shape[0]
-        n_kde=y.shape[1]
+        n_comp=x_kde.shape[0]
+        n_kde=x_kde.shape[1]
         c_=(n_kde*(dim+2))/4
         c=torch.as_tensor(c_).pow(2/(dim+4)).to(device)  
-        H=prec*(y.var(1)/c).clamp(torch.finfo().eps,float('inf'))
+        H=(x_kde.var(1) / c).clamp(torch.finfo().eps, float('inf'))
 
-        d=((y.view(n_comp,n_kde,1,dim)-x.view(1,1,n_ed,dim))**2)
+        d=((x_kde.view(n_comp, n_kde, 1, dim) - x.view(1, 1, n_ed, dim)) ** 2)
         H_=H.view(n_comp,dim,1,1).inverse().view(n_comp,1,1,dim)
         const=0.5*H.log().sum(1)+0.5*dim*torch.tensor(2*math.pi).log()
         const=const.view(n_comp,1,1)
@@ -136,11 +134,11 @@ def get_NNE(device):
         return torch.log(N) - torch.digamma(K) + lcd + d/nb_samples*torch.sum(torch.log(a))
     return NNE
 
-def get_entropy(kNNE,n_samples_ED,n_samples_KDE,KDE_prec,n_samples_NNE,device):
+def get_entropy(kNNE,n_samples_ED,n_samples_KDE,n_samples_NNE,device):
     if kNNE == 0:
         def entropy(GeN):
             KDE=get_KDE(device)
-            return -KDE(GeN(n_samples_ED), GeN.sample(n_samples_KDE), KDE_prec).mean()
+            return -KDE(GeN(n_samples_ED), GeN.sample(n_samples_KDE)).mean()
         return entropy
     else:
         def entropy(GeN):
@@ -148,151 +146,55 @@ def get_entropy(kNNE,n_samples_ED,n_samples_KDE,KDE_prec,n_samples_NNE,device):
             return NNE(GeN(n_samples_NNE), kNNE)
         return entropy
 
-def getParser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--ensemble_size", type=int, default=1,
-                        help="number of hypernets to train in the ensemble")
-    parser.add_argument("--lat_dim", type=int, default=5,
-                        help="number of latent dimensions of each hypernet")
-    parser.add_argument("--layerwidth", type=int, default=50,
-                        help="layerwidth of each hypernet")
-    parser.add_argument("--init_w", type=float, default=0.2,
-                        help="std for weight initialization of output layers")
-    #    parser.add_argument("--init_b", type=float, default=0.000001,
-    #                        help="std for bias initialization of output layers")
-    parser.add_argument("--NNE", type=int, default=0,
-                        help="k≥1 Nearest Neighbor Estimate, 0 is for KDE")
-    parser.add_argument("--n_samples_NNE", type=int, default=500,
-                        help="number of samples for NNE")
-    parser.add_argument("--n_samples_KDE", type=int, default=500,
-                        help="number of samples for KDE")
-    parser.add_argument("--n_samples_ED", type=int, default=50,
-                        help="number of samples for MC estimation of differential entropy")
-    parser.add_argument("--n_samples_LP", type=int, default=100,
-                        help="number of samples for MC estimation of expected logposterior")
-    parser.add_argument("--max_iter", type=int, default=100000,
-                        help="maximum number of learning iterations")
-    parser.add_argument("--learning_rate", type=float, default=0.08,
-                        help="initial learning rate of the optimizer")
-    parser.add_argument("--min_lr", type=float, default=0.00000001,
-                        help="minimum learning rate triggering the end of the optimization")
-    parser.add_argument("--patience", type=int, default=400,
-                        help="scheduler patience")
-    parser.add_argument("--lr_decay", type=float, default=.5,
-                        help="scheduler multiplicative factor decreasing learning rate when patience reached")
-    parser.add_argument("--device", type=str, default=None,
-                        help="force device to be used")
-    parser.add_argument("--verbose", type=bool, default=False,
-                        help="force device to be used")
-    parser.add_argument("--show_metrics", type=bool, default=False,
-                        help="log metrics during training")
-    return parser
 
 
-def main(get_data, get_model, sigma_noise, experiment_name,
-         ensemble_size, lat_dim, HN_layerwidth, init_w, kNNE, n_samples_NNE, n_samples_KDE, n_samples_ED, n_samples_LP,
-         max_iter, learning_rate, min_lr, patience, lr_decay,
-         device=None, verbose=False,show_metrics=False,init_b=.001,HN_activation=nn.ReLU(),KDE_prec=1.):
-    
-    xpname = experiment_name+'/GeNVI'             
-    mlflow.set_experiment(xpname)
-    expdata = mlflow.get_experiment_by_name(xpname)
-    
-    if kNNE==0:
-            entropy_mthd='KDE'
-    else:
-            entropy_mthd=str(kNNE)+'NNE'
-    
-    with mlflow.start_run():
-        mlflow.set_tag('device', device) 
-        mlflow.set_tag('method', entropy_mthd)
+def GeNVI(objective_fn,
+          GeN, kNNE, n_samples_NNE, n_samples_KDE, n_samples_ED, n_samples_LP,
+          max_iter, learning_rate, min_lr, patience, lr_decay,
+          device=None, verbose=False):
 
-        param_count, mlp=get_model()  
-        
-        mlflow.log_param('sigma noise', sigma_noise)
-        mlflow.set_tag('dimensions', param_count)
+    """
+    GeNVI method
 
-        mlflow.log_param('ensemble_size', ensemble_size)
-        mlflow.log_param('HyperNet_lat_dim', lat_dim)
-        mlflow.log_param('HyperNet_layerwidth', HN_layerwidth)
+        objective_fn : S X DIM -> S
 
-        mlflow.log_param('learning_rate', learning_rate)
-        mlflow.log_param('patience', patience)
-        mlflow.log_param('lr_decay', lr_decay)
+        GeN (nn.Module) with methods:
+            forward(N): sample of size  N X DIM
+            sample(N): ensemble sample of size   ENS X N X DIM
+            # TODO: add description
+            _save_best_model
+            _get_best_model
 
-        mlflow.log_param('max_iter', max_iter)
-        mlflow.log_param('min_lr', min_lr)
-        
-        mlflow.log_param('n_samples_KDE', n_samples_KDE)
-        mlflow.log_param('n_samples_ED', n_samples_ED)
-        mlflow.log_param('n_samples_LP', n_samples_LP)
-        
+    """
 
-        entropy=get_entropy(kNNE, n_samples_ED,n_samples_KDE,KDE_prec,n_samples_NNE,device)
+    entropy = get_entropy(kNNE, n_samples_ED, n_samples_KDE, n_samples_NNE, device)
 
 
-        X_train, y_train, y_train_un, X_test, y_test_un, inverse_scaler_y = get_data(device)
-        logtarget=get_logposterior(mlp, X_train, y_train, sigma_noise, device)
+    optimizer = torch.optim.Adam(GeN.parameters(), lr=learning_rate)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=patience, factor=lr_decay)
 
-        Hyper_Nets=HyNetEns(ensemble_size, lat_dim, HN_layerwidth, param_count, HN_activation, init_w, init_b, device)
+    for t in range(max_iter):
+        optimizer.zero_grad()
 
-        optimizer = torch.optim.Adam(Hyper_Nets.parameters(), lr=learning_rate)
-        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=patience, factor=lr_decay)
+        ED = entropy(GeN)
+        LP = objective_fn(GeN(n_samples_LP)).mean()
+        L = -ED - LP
+        L.backward()
 
-
-        for t in range(max_iter):
-            optimizer.zero_grad()
-
-
-            ED=entropy(Hyper_Nets)
-            LP=logtarget(Hyper_Nets(n_samples_LP)).mean()
-            L =-ED-LP
-            L.backward()
+        lr = optimizer.param_groups[0]['lr']
 
 
-            lr = optimizer.param_groups[0]['lr']
+        scheduler.step(L.detach().clone().cpu().numpy())
 
+        if verbose:
+            stats = 'Epoch [{}/{}], Training Loss: {}, Learning Rate: {}'.format(t, max_iter, L, lr)
+            print(stats)
 
+        GeN._save_best_model(L, t, ED, LP)
 
+        if lr < min_lr:
+            break
 
+        optimizer.step()
 
-            if show_metrics:
-                mlflow.log_metric("tr ELBO", float(L.detach().clone().cpu().numpy()), t)
-                mlflow.log_metric("tr -log posterior", float(-LP.detach().clone().cpu().numpy()), t)
-                mlflow.log_metric("tr differential entropy", float(ED.detach().clone().cpu().numpy()), t)
-                mlflow.log_metric("tr learning rate", float(lr), t)
-
-                #to log metrics
-                #theta=Hyper_Nets(100).detach()
-                #log_metrics(theta, mlp, X_train, y_train_un, X_test, y_test_un, sigma_noise, inverse_scaler_y, t,device)
-
-
-
-            scheduler.step(L.detach().clone().cpu().numpy())
-
-            if verbose:
-                stats = 'Epoch [{}/{}], Training Loss: {}, Learning Rate: {}'.format(t, max_iter, L, lr)
-                print(stats)
-
-            Hyper_Nets._save_best_model(L,t,ED,LP)
-
-            if lr < min_lr:
-                break
-
-
-            optimizer.step()
-
-        Epoch,ELBO,ED,LP=Hyper_Nets._get_best_model()
-
-
-        theta=Hyper_Nets(1000).detach()
-        log_metrics(theta, mlp, X_train, y_train_un, X_test, y_test_un, sigma_noise, inverse_scaler_y, t,device)
-        mlflow.log_metric("ELBO", float(ELBO.detach().clone().cpu().numpy()), t)
-        mlflow.log_metric("Epoch", Epoch, t)
-
-
-        mlflow.log_metric("-log posterior", float(-LP.detach().clone().cpu().numpy()), t)
-        mlflow.log_metric("differential entropy", float(ED.detach().clone().cpu().numpy()), t)
-        mlflow.pytorch.log_model(Hyper_Nets,'models')
-
-
+    return
