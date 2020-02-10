2d4ef2975f56479bd9d33659b16c346c97fb0693
Yann Pequignot <yann.pequignot@gmail.com>
2020-02-10 14:41:51 -0500

    cleaning Tools

--- Metrics/__init__.py
+++ Metrics/__init__.py
@@ -7,6 +7,23 @@ import torch
 #     se=(y_pred-y.view(n_samples,1))**2
 #     return torch.std_mean(se)
 
+def MSE(theta,model,X,y,inv_transform,device):
+    n_samples=x.shape[0]
+    y_pred =inv_transform(model(x, theta)).mean(0)
+    se=(y_pred-y.view(n_samples,1))**2
+    return torch.std_mean(se)
+
+#NLPD from Quinonero-Candela and al.
+# average Negative Log Likelihood
+# the average negative log predictive density (NLPD) of the true targets
+
+def NLPD(theta,model, x, y, sigma_noise,inv_transform,device):
+    y_pred =inv_transform(model(x, theta))
+    L = _log_norm(y_pred, y, sigma_noise,device)
+    n_x = torch.as_tensor(float(x.shape[0]), device=device)
+    n_theta = torch.as_tensor(float(theta.shape[0]), device=device)
+    log_posterior_predictive = torch.logsumexp(L, 0) - torch.log(n_theta)
+    return torch.std_mean(-log_posterior_predictive)
 
 def avgNLL(loglikelihood, theta, X, y):
     L = loglikelihood(theta, X, y)
