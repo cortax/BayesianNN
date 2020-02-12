64505bb66bc0aa9bd7b4d157f71ffb3b1937802d
Yann <yann.pequignot@gmail.com>
2020-02-12 18:38:51 -0500

    test metrics on cpu

--- Experiments/__init__.py
+++ Experiments/__init__.py
@@ -17,13 +17,13 @@ class AbstractRegressionSetup(ABC):
 
     def evaluate_metrics(self, theta,device):
         theta = theta.to(device)
-        nLPP_train = nLPP(self._loglikelihood, theta, self._X_train, self._y_train,device)
-        nLPP_validation = nLPP(self._loglikelihood, theta, self._X_validation, self._y_validation,device)
-        nLPP_test = nLPP(self._loglikelihood, theta, self._X_test, self._y_test,device)
+        nLPP_train = nLPP(self._loglikelihood, theta, self._X_train, self._y_train.to(device),device)
+        nLPP_validation = nLPP(self._loglikelihood, theta, self._X_validation, self._y_validation.to(device),device)
+        nLPP_test = nLPP(self._loglikelihood, theta, self._X_test, self._y_test.to(device),device)
 
-        RSE_train = RSE(self._normalized_prediction, theta, self._X_train, self._y_train,device)
-        RSE_validation = RSE(self._normalized_prediction, theta, self._X_validation, self._y_validation,device)
-        RSE_test = RSE(self._normalized_prediction, theta, self._X_test, self._y_test,device)
+        RSE_train = RSE(self._normalized_prediction, theta, self._X_train, self._y_train.to(device),device)
+        RSE_validation = RSE(self._normalized_prediction, theta, self._X_validation.to(device), self._y_validation,device)
+        RSE_test = RSE(self._normalized_prediction, theta, self._X_test, self._y_test.to(device),device)
         return nLPP_train, nLPP_validation, nLPP_test, RSE_train, RSE_validation, RSE_test
     # @abstractmethod
     # def evaluate(self):
