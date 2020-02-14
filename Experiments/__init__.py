847153e244aed297a0cdb62e306d63f3aa6ada0d
Yann <yann.pequignot@gmail.com>
2020-02-14 12:44:46 -0500

    Auto stash before merge of "NewPaperExperiment" and "origin/NewPaperExperiment"

--- Experiments/__init__.py
+++ Experiments/__init__.py
@@ -21,7 +21,8 @@ def switch_setup(setup):
         'california': importlib.util.spec_from_file_location("california", "Experiments/california/__init__.py"),
         'concrete': importlib.util.spec_from_file_location("concrete", "Experiments/concrete/__init__.py"),
         'wine': importlib.util.spec_from_file_location("wine", "Experiments/winequality/__init__.py"),
-        'kin8nm': importlib.util.spec_from_file_location("kin8nm", "Experiments/kin8nm/__init__.py")
+        'kin8nm': importlib.util.spec_from_file_location("kin8nm", "Experiments/kin8nm/__init__.py"),
+        'powerplant': importlib.util.spec_from_file_location("powerplant", "Experiments/ccpowerplant/__init__.py")
     }[setup]
 
 def get_setup(setup,device):
