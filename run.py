d08579777505b0a28e0e25fe4283ba52c5e9b603
Mathieu <mathieu.alain.2@ulaval.ca>
2020-02-14 16:36:49 -0500

    fix run.py maintenant 2 a 2

--- run.py
+++ run.py
@@ -3,12 +3,9 @@ from multiprocessing import Pool
 
 if __name__ == "__main__":
     
-    datasets = [("foong", "0"), ("boston", "1"), ("wine", "0"), ("concrete", "1"), ("california","0")]
-    
-#    datasets_1 = [("foong", "0"), ("boston", "1"), ("wine", "0")]
-#    datasets_2 = [("concrete", "1"), ("california","0")]
-    
-    MAPs = ["-m Experiments.MAP --max_iter=10 --ensemble_size=1 --verbose=1 --device=cuda:" + j + " --setup=" + i for i,j in datasets]
+    datasets = [("foong", "0"), ("boston", "1"), ("wine", "0"), ("concrete", "1"), ("california","0"), ("kin8nm","1"), ("ccpowerplant", "0"), ("yatch", "1")]
+
+    MAPs = ["-m Experiments.MAP --max_iter=500 --ensemble_size=1000 --verbose=1 --device=cuda:" + j + " --setup=" + i for i,j in datasets]
     
     MFVIs = ["-m Experiments.MFVI --max_iter=10 --init_std=1. --learning_rate=0.1 --min_lr=0.000001 --patience=100 --lr_decay=0.5 --n_ELBO_samples=100 --verbose=1 --device=cuda:" + j + " --setup=" + i for i,j in datasets]
     
@@ -18,11 +15,22 @@ if __name__ == "__main__":
     
     def run_dataset(algorithm):                                                             
         os.system('python {}'.format(algorithm))                                                                                                         
-                                                                                
-    pool = Pool(processes=len(datasets_1)) 
-    
-    pool.map(run_dataset, MAPs)
-#    pool.map(run_dataset, MFVIs)   
-#    pool.map(run_dataset, GeNVIs)   
-#    pool.map(run_dataset, PTMCMCs)   
+ 
    
+    pool = Pool(processes=2) 
+    pool.map(run_dataset, MAPs[0:2])
+    pool = Pool(processes=2) 
+    pool.map(run_dataset, MAPs[2:4])   
+
+    pool = Pool(processes=2) 
+    pool.map(run_dataset, MFVIs[0:2])  
+    pool = Pool(processes=2) 
+    pool.map(run_dataset, MFVIs[2:4])   
+    
+    pool = Pool(processes=2) 
+    pool.map(run_dataset, GeNVIs[0:2])  
+    pool = Pool(processes=2) 
+    pool.map(run_dataset, GeNVIs[2:4])   
+
+#    pool.map(run_dataset, PTMCMCs)   
+
