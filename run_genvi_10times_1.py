80548ddccc198c1b22362f67853b504a5b27900a
Mathieu <mathieu.alain.2@ulaval.ca>
2020-02-18 20:42:12 -0500

    add run genvi 10 times

---
+++ run_genvi_10times_1.py
@@ -0,0 +1,24 @@
+import os
+from multiprocessing import Pool
+
+if __name__ == "__main__":
+
+   
+    # (datasets, GPU)
+    datasets = [("foong", "0"), ("boston", "1"), ("wine", "0"), ("concrete", "1"), ("kin8nm", "0"), ("yacht", "1"), ("powerplant", "0")]
+
+    GeNVIs = ["-m Experiments.GeNVI --max_iter=50000 --n_samples_ED=50 --n_samples_LP=100 --lat_dim=5 --layerwidth=50 --learning_rate=0.08 --patience=1000 --lr_decay=.8 --verbose=1 --device=cuda:" + j + " --setup=" + i for i,j in datasets]
+    
+    
+    def run_dataset(algorithm):                                                             
+        os.system('python {}'.format(algorithm))                                                                                                         
+    
+    for i in range(0,10):
+        pool = Pool(processes=2) 
+        pool.map(run_dataset, GeNVIs[0:2])  
+        pool = Pool(processes=2) 
+        pool.map(run_dataset, GeNVIs[2:4])   
+        pool = Pool(processes=2) 
+        pool.map(run_dataset, GeNVIs[4:6])   
+        pool = Pool(processes=1) 
+        pool.map(run_dataset, GeNVIs[6:7])
