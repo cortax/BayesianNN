5f703fd923f443179293152f63d34a67330323fc
Yann <yann.pequignot@gmail.com>
2020-03-05 19:25:49 -0500

    New GeNVI-PAC inference method
    Fonctionne seulement sur données Foong

---
+++ Experiments/GeNVI-PAC.py
@@ -0,0 +1,164 @@
+from torch import nn
+import argparse
+import mlflow
+import timeit
+
+from tempfile import TemporaryDirectory
+
+from Inference.GeNVI_PAC_method import GeNVariationalInference, GeNetEns
+from Experiments import log_exp_metrics, draw_experiment, get_setup, save_model
+
+import tempfile
+
+
+## command line example
+# python -m Experiments.GeNVI --setup=foong --verbose=True --max_iter=20000 --learning_rate=0.05 --lat_dim= --layerwidth=
+
+def GeNVI_learning(loss,prior,n_samples,
+                   ensemble_size, lat_dim, layerwidth, param_count, activation, init_w, init_b,
+                   kNNE, n_samples_NNE, n_samples_KDE, n_samples_ED, n_samples_LP,
+                   max_iter, learning_rate, min_lr, patience, lr_decay,
+                   device=None, verbose=False):
+	GeN = GeNetEns(ensemble_size, lat_dim, layerwidth, param_count,
+	               activation, init_w, init_b, device)
+
+	with TemporaryDirectory() as temp_dir:
+		optimizer = GeNVariationalInference(loss,prior,n_samples,
+		                                    kNNE, n_samples_NNE, n_samples_KDE, n_samples_ED, n_samples_LP,
+		                                    max_iter, learning_rate, min_lr, patience, lr_decay,
+		                                    device, verbose, temp_dir)
+		the_epoch, the_scores = optimizer.run(GeN)
+
+	log_scores = [optimizer.score_elbo, optimizer.score_entropy, optimizer.score_logposterior, optimizer.score_lr]
+	return GeN, the_epoch, the_scores, log_scores
+
+
+def log_GeNVI_experiment(setup, the_epoch, the_scores, log_scores,
+                         ensemble_size, lat_dim, layerwidth, param_count, init_w,
+                         kNNE, n_samples_NNE, n_samples_KDE, n_samples_ED, n_samples_LP,
+                         max_iter, learning_rate, min_lr, patience, lr_decay,
+                         device):
+
+
+	mlflow.set_tag('device', device)
+	mlflow.set_tag('sigma noise', setup.sigma_noise)
+	mlflow.set_tag('dimensions', setup.param_count)
+
+	if kNNE == 0:
+		entropy_mthd = 'KDE'
+	else:
+		entropy_mthd = str(kNNE) + 'NNE'
+
+	mlflow.set_tag('entropy', entropy_mthd)
+
+	mlflow.log_param('ensemble_size', ensemble_size)
+	mlflow.log_param('lat_dim', lat_dim)
+	mlflow.log_param('layerwidth', layerwidth)
+	mlflow.log_param('init_w', init_w)
+
+	mlflow.log_param('n_samples_NNE', n_samples_NNE)
+	mlflow.log_param('n_samples_KDE', n_samples_KDE)
+	mlflow.log_param('n_samples_ED', n_samples_ED)
+	mlflow.log_param('n_samples_LP', n_samples_LP)
+
+	mlflow.log_param('learning_rate', learning_rate)
+	mlflow.log_param('patience', patience)
+	mlflow.log_param('lr_decay', lr_decay)
+	mlflow.log_param('max_iter', max_iter)
+	mlflow.log_param('min_lr', min_lr)
+
+	mlflow.log_metric('The epoch', the_epoch)
+
+	mlflow.log_metric("The elbo", float(the_scores[0]))
+	mlflow.log_metric("The entropy", float(the_scores[1]))
+	mlflow.log_metric("The logposterior", float(the_scores[2]))
+
+	for t in range(len(log_scores[0])):
+		mlflow.log_metric("elbo", float(log_scores[0][t]), step=100*t)
+		mlflow.log_metric("entropy", float(log_scores[1][t]), step=100*t)
+		mlflow.log_metric("logposterior", float(log_scores[2][t]), step=100*t)
+		mlflow.log_metric("learning_rate", float(log_scores[3][t]), step=100*t)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--setup", type=str, default=None,
+                    help="data setup on which run the method")
+parser.add_argument("--ensemble_size", type=int, default=1,
+                    help="number of hypernets to train in the ensemble")
+parser.add_argument("--lat_dim", type=int, default=5,
+                    help="number of latent dimensions of each hypernet")
+parser.add_argument("--layerwidth", type=int, default=50,
+                    help="layerwidth of each hypernet")
+parser.add_argument("--init_w", type=float, default=0.2,
+                    help="std for weight initialization of output layers")
+#    parser.add_argument("--init_b", type=float, default=0.000001,
+#                        help="std for bias initialization of output layers")
+parser.add_argument("--EntropyE", type=int, default=0,
+                    help="k≥1 Nearest Neighbor Estimate, 0 is for KDE")
+parser.add_argument("--n_samples_NNE", type=int, default=500,
+                    help="number of samples for NNE")
+parser.add_argument("--n_samples_KDE", type=int, default=1000,
+                    help="number of samples for KDE")
+parser.add_argument("--n_samples_ED", type=int, default=50,
+                    help="number of samples for MC estimation of differential entropy")
+parser.add_argument("--n_samples_LP", type=int, default=100,
+                    help="number of samples for MC estimation of expected logposterior")
+parser.add_argument("--max_iter", type=int, default=10000,
+                    help="maximum number of learning iterations")
+parser.add_argument("--learning_rate", type=float, default=0.08,
+                    help="initial learning rate of the optimizer")
+parser.add_argument("--min_lr", type=float, default=0.00000001,
+                    help="minimum learning rate triggering the end of the optimization")
+parser.add_argument("--patience", type=int, default=400,
+                    help="scheduler patience")
+parser.add_argument("--lr_decay", type=float, default=.5,
+                    help="scheduler multiplicative factor decreasing learning rate when patience reached")
+parser.add_argument("--device", type=str, default=None,
+                    help="force device to be used")
+parser.add_argument("--verbose", type=bool, default=False,
+                    help="force device to be used")
+
+if __name__ == "__main__":
+	# python -m Experiments.boston.GeNVI --max_iter=10 --verbose=True --device='cpu'
+
+	args = parser.parse_args()
+	print(args)
+
+	setup = get_setup(args.setup, args.device)
+
+	activation = nn.ReLU()
+	init_b = .001
+
+	start = timeit.default_timer()
+	GeN, the_epoch, the_scores, log_scores = GeNVI_learning(setup.loss,setup.logprior,setup.n_samples,
+	                                                        args.ensemble_size, args.lat_dim, args.layerwidth,
+	                                                        setup.param_count,
+	                                                        activation, args.init_w, init_b,
+	                                                        args.EntropyE, args.n_samples_NNE, args.n_samples_KDE,
+	                                                        args.n_samples_ED, args.n_samples_LP,
+	                                                        args.max_iter, args.learning_rate, args.min_lr,
+	                                                        args.patience, args.lr_decay,
+	                                                        args.device, args.verbose)
+	stop = timeit.default_timer()
+	execution_time = stop - start
+
+	xpname = setup.experiment_name + '/GeNVI'
+	mlflow.set_experiment(xpname)
+
+	with mlflow.start_run():
+		log_GeNVI_experiment(setup, the_epoch, the_scores, log_scores,
+		                     args.ensemble_size, args.lat_dim, args.layerwidth, setup.param_count, args.init_w,
+		                     args.EntropyE, args.n_samples_NNE, args.n_samples_KDE, args.n_samples_ED,
+		                     args.n_samples_LP,
+		                     args.max_iter, args.learning_rate, args.min_lr, args.patience, args.lr_decay,
+		                     args.device)
+
+		log_device = 'cpu'
+		theta = GeN(1000).detach().to(log_device)
+		log_exp_metrics(setup.evaluate_metrics, theta, execution_time, log_device)
+
+		save_model(GeN)
+
+		if setup.plot:
+#			theta_ens = GeN(1000).detach()#.cpu()
+			draw_experiment(setup.makePlot, theta, log_device)
---
+++ Inference/GeNVI_PAC_method.py
@@ -0,0 +1,303 @@
+import torch
+from torch import nn
+import math
+
+
+
+
+class GeNet(nn.Module):
+            def __init__(self, lat_dim, nb_neur, output_dim,  activation, init_w, init_b, device):
+                super(GeNet, self).__init__()
+                self.lat_dim = lat_dim
+                self.device=device
+                self.output_dim=output_dim
+                self.hnet=nn.Sequential(
+                        nn.Linear(lat_dim,nb_neur),
+                        activation,
+                        nn.Linear(nb_neur,output_dim)
+                        ).to(device)
+                
+                torch.nn.init.normal_(self.hnet[2].weight,mean=0., std=init_w)
+                torch.nn.init.normal_(self.hnet[2].bias,mean=0., std=init_b)
+    
+            def forward(self, n=1):
+                epsilon = torch.randn(size=(n,self.lat_dim), device=self.device)
+                return self.hnet(epsilon)           
+
+class GeNetEns(nn.Module):
+    def __init__(self, nb_comp, lat_dim, layer_width, output_dim, activation, init_w, init_b, device):
+        super(GeNetEns, self).__init__()
+        self.device = device
+        self.nb_comp=nb_comp
+        self.output_dim=output_dim
+        self.components= nn.ModuleList([GeNet(lat_dim,layer_width,output_dim,activation,init_w,init_b,device) for i in range(nb_comp)]).to(device)
+
+        self._best_compnents = None
+        self._best_score = float('inf')
+
+    def sample(self, n=1):
+        return torch.stack([self.components[c](n) for c in range(self.nb_comp)])
+
+
+    def forward(self, n=1):
+        d = torch.distributions.multinomial.Multinomial(n, torch.ones(self.nb_comp))
+        m = d.sample()
+        return torch.cat([self.components[c](int(m[c])) for c in range(len(self.components))])
+    
+    
+"""
+use:
+
+Hyper_Nets=HyNetEns(ensemble_size,lat_dim,HN_layerwidth, output_dim,activation,init_w,init_b).to(device)
+
+Hyper_Nets(100)
+
+Hyper_Nets.sample(100)
+
+"""
+
+
+
+### Entropy approximation
+
+def get_KDE(device):
+    def KDE(x, x_kde):
+        """
+        KDE    
+
+        Parameters:
+            x (Tensor): Inputs, NbExemples X NbDimensions   
+            x_kde (Tensor):  Batched samples, NbBatch x NbSamples X NbDimensions
+
+
+        Returns:
+            (Tensor) KDE log estimate for x based on batched diagonal "Silverman's rule of thumb", NbExemples
+            See Wand and Jones p.111 "Kernel Smoothing" 1995.  
+
+        """
+
+        dim=x.shape[-1]
+        n_ed=x.shape[0]
+        n_comp=x_kde.shape[0]
+        n_kde=x_kde.shape[1]
+        c_=(n_kde*(dim+2))/4
+        c=torch.as_tensor(c_).pow(2/(dim+4)).to(device)  
+        H=(x_kde.var(1) / c).clamp(torch.finfo().eps, float('inf'))
+
+        d=((x_kde.view(n_comp, n_kde, 1, dim) - x.view(1, 1, n_ed, dim)) ** 2)
+        H_=H.view(n_comp,dim,1,1).inverse().view(n_comp,1,1,dim)
+        const=0.5*H.log().sum(1)+0.5*dim*torch.tensor(2*math.pi).log()
+        const=const.view(n_comp,1,1)
+        ln=-0.5*(H_*d).sum(3)-const
+        N=torch.as_tensor(float(n_comp*n_kde),device=device)
+        return (ln.logsumexp(0).logsumexp(0)-torch.log(N)).unsqueeze(-1)
+    return KDE
+
+def get_NNE(device):
+    def NNE(theta,k=1):
+        """
+        Parameters:
+            theta (Tensor): Samples, NbExemples X NbDimensions   
+            k (Int): ordinal number
+
+        Returns:
+            (Float) k-Nearest Neighbour Estimation of the entropy of theta  
+
+        """
+        nb_samples=theta.shape[0]
+        dim=theta.shape[1]
+        D=torch.cdist(theta,theta)
+        a = torch.topk(D, k=k+1, dim=0, largest=False, sorted=True)[0][k].clamp(torch.finfo().eps,float('inf')).to(device)
+        d=torch.as_tensor(float(dim),device=device)
+        K=torch.as_tensor(float(k),device=device)
+        N=torch.as_tensor(float(nb_samples),device=device)
+        pi=torch.as_tensor(math.pi,device=device)
+        lcd = d/2.*pi.log() - torch.lgamma(1. + d/2.0)
+        return torch.log(N) - torch.digamma(K) + lcd + d/nb_samples*torch.sum(torch.log(a))
+    return NNE
+
+def get_entropy(kNNE,n_samples_ED,n_samples_KDE,n_samples_NNE,device):
+    if kNNE == 0:
+        def entropy(GeN):
+            KDE=get_KDE(device)
+            return -KDE(GeN(n_samples_ED), GeN.sample(n_samples_KDE)).mean()
+        return entropy
+    else:
+        def entropy(GeN):
+            NNE=get_NNE(device)
+            return NNE(GeN(n_samples_NNE), kNNE)
+        return entropy
+
+
+
+
+
+
+def KDE(x, x_kde,device):
+    """
+    KDE
+
+    Parameters:
+        x (Tensor): Inputs, NbSamples X NbDimensions
+        x_kde (Tensor):  Batched samples, NbBatch x NbSamples X NbDimensions
+
+
+    Returns:
+        (Tensor) KDE log estimate for x based on batched diagonal "Silverman's rule of thumb", NbExemples
+        See Wand and Jones p.111 "Kernel Smoothing" 1995.
+
+    """
+
+    dim=x.shape[-1]
+    n_ed=x.shape[0]
+    n_comp=x_kde.shape[0]
+    n_kde=x_kde.shape[1]
+    c_=(n_kde*(dim+2))/4
+    c=torch.as_tensor(c_).pow(2/(dim+4)).to(device)
+    H=(x_kde.var(1) / c).clamp(torch.finfo().eps, float('inf'))
+
+    d=((x_kde.view(n_comp, n_kde, 1, dim) - x.view(1, 1, n_ed, dim)) ** 2)
+    H_=H.view(n_comp,dim,1,1).inverse().view(n_comp,1,1,dim)
+    const=0.5*H.log().sum(1)+0.5*dim*torch.tensor(2*math.pi).log()
+    const=const.view(n_comp,1,1)
+    ln=-0.5*(H_*d).sum(3)-const
+    N=torch.as_tensor(float(n_comp*n_kde), device=device)
+    return (ln.logsumexp(0).logsumexp(0)-torch.log(N)).unsqueeze(-1)
+
+def NNE(theta,device,k=1):
+    """
+    Parameters:
+        theta (Tensor): Samples, NbExemples X NbDimensions
+        k (Int): ordinal number
+
+    Returns:
+        (Float) k-Nearest Neighbour Estimation of the entropy of theta
+
+    """
+    nb_samples=theta.shape[0]
+    dim=theta.shape[1]
+    D=torch.cdist(theta,theta)
+    a = torch.topk(D, k=k+1, dim=0, largest=False, sorted=True)[0][k].clamp(torch.finfo().eps,float('inf')).to(device)
+    d=torch.as_tensor(float(dim), device=device)
+    K=torch.as_tensor(float(k), device=device)
+    N=torch.as_tensor(float(nb_samples), device=device)
+    pi=torch.as_tensor(math.pi, device=device)
+    lcd = d/2.*pi.log() - torch.lgamma(1. + d/2.0)
+    return torch.log(N) - torch.digamma(K) + lcd + d/nb_samples*torch.sum(torch.log(a))
+
+
+class GeNVariationalInference():
+    def __init__(self, loss,prior, n_data_samples,
+                 kNNE, n_samples_NNE, n_samples_KDE, n_samples_ED, n_samples_LP,
+                 max_iter, learning_rate, min_lr, patience, lr_decay,
+                 device, verbose, temp_dir, save_best=True):
+        self.loss = loss
+        self.prior=prior
+        self.n_data_samples=n_data_samples
+        self.kNNE=kNNE
+        self.n_samples_NNE=n_samples_NNE
+        self.n_samples_KDE=n_samples_KDE
+        self.n_samples_ED=n_samples_ED
+        self.n_samples_LP=n_samples_LP
+        self.max_iter = max_iter
+        self.learning_rate = learning_rate
+        self.min_lr = min_lr
+        self.patience = patience
+        self.lr_decay = lr_decay
+        self.device = device
+        self.verbose = verbose
+
+        self.save_best=save_best
+        self._best_score=float('inf')
+
+
+        self.tempdir_name = temp_dir
+
+
+
+    def _entropy(self, GeN):
+        if self.kNNE == 0:
+            ED = -KDE(GeN(self.n_samples_ED), GeN.sample(self.n_samples_KDE),self.device).mean()
+        else:
+            ED = NNE(GeN(self.n_samples_NNE),k=self.kNNE, device=self.device)
+        return ED
+
+
+
+    def _save_best_model(self, GeN, epoch, score,ED,C):
+        if score < self._best_score:
+            torch.save({
+                'epoch': epoch,
+                'state_dict': GeN.state_dict(),
+                'ELBO': score,
+                'ED':ED,
+                'Temp':C
+            }, self.tempdir_name+'/best.pt')
+            self._best_score=score
+
+    def _get_best_model(self, GeN):
+        best= torch.load(self.tempdir_name+'/best.pt')
+        GeN.load_state_dict(best['state_dict'])
+        return best['epoch'], [best['ELBO'], best['ED'], best['Temp']]
+
+    def run(self, GeN, show_fn=None):
+        _C=torch.tensor(1., requires_grad=True)
+        optimizer = torch.optim.Adam([_ for _ in GeN.parameters()]+[_C], lr=self.learning_rate)
+
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=self.patience,
+                                                               factor=self.lr_decay)
+
+
+        self.score_elbo = []
+        self.score_entropy = []
+        self.score_logposterior = []
+        self.score_lr = []
+
+        for t in range(self.max_iter):
+            optimizer.zero_grad()
+
+            C = torch.log(torch.exp(_C) + 1.)
+            delta=0.05
+
+            ED = self._entropy(GeN)
+            nlloss = self.loss(GeN(self.n_data_samples)).mean()
+            kl=ED- self.prior(GeN(self.n_samples_ED)).mean()
+
+            L = (1 / (1 - torch.exp(-C))) * \
+                  (1 - torch.exp(-C * nlloss - (1 / self.n_data_samples) * \
+                  (kl + math.log(2 * math.sqrt(self.n_data_samples) / delta))))
+
+            L.backward()
+
+            lr = optimizer.param_groups[0]['lr']
+
+            scheduler.step(L.detach().clone().cpu().numpy())
+
+            if self.verbose:
+                stats = 'Epoch [{}/{}], Loss: {}, Entropy {}, Temp: {}, Learning Rate: {}'.format(t, self.max_iter, L, ED, C, lr)
+                print(stats)
+
+            if t % 100 ==0:
+                self.score_elbo.append(L.detach().clone().cpu())
+                self.score_entropy.append(ED.detach().clone().cpu())
+                self.score_logposterior.append(nlloss.detach().clone().cpu())
+                self.score_lr.append(lr)
+
+                if show_fn is not None:
+                    #print('show')
+                    show_fn(GeN,500)
+
+            if self.save_best:
+                self._save_best_model(GeN, t,L.detach().clone(), ED.detach().clone(), C.detach().clone())
+
+            if lr < self.min_lr:
+                self._save_best_model(GeN, t, L.detach().clone(), ED.detach().clone(), C.detach().clone())
+                break
+
+            if t+1==self.max_iter:
+                self._save_best_model(GeN, t, L.detach().clone(), ED.detach().clone(), C.detach().clone())
+
+            optimizer.step()
+
+        best_epoch, scores =self._get_best_model(GeN)
+        return best_epoch, scores
