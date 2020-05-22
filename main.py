59a8f2cbd74b79de9df2a513fbb7fbf44200ead5
Mathieu Alain <maala89@gra-login1.graham.sharcnet>
2020-05-22 01:27:40 -0400

    bug fix removal space between values in array

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ if __name__ == "__main__":
         with open('array', 'r') as array:
                 lines = array.read().splitlines()
 
-        index = int(sys.argv[1])
+        index = int(sys.argv[1]) + 1
         time = str(sys.argv[2])
-        line = lines[index+1].split(',')
+        line = lines[index].split(',')
         dataset = str(line[0])
         num_iter = str(line[1])
         burning = str(line[2])
         thinning = str(line[3])
 
-        os.system('python -m Experiments.HMC --setup='+ dataset + ' --num_iter=' + num_iter + ' --burning=' + burning + ' --thinning=' + thinning + ' --max_time='+ time)
+        os.system('python -m Experiments.HMC --setup='+ dataset + ' --numiter=' + num_iter + ' --burning=' + burning + ' --thinning=' + thinning + ' --max_time='+ time)
