b01fad1b67a90b97fc421280f1f9596af2b71922
yannpequignot <yann.pequignot@gmail.com>
2020-06-01 14:26:37 -0400

    update

--- runs_Yann-metrics.py
+++ runs_Yann-metrics.py
@@ -43,10 +43,10 @@ def run(dataset, method):
         PICPs.append(PICP_test.item())
         MPIWs.append(MPIW_test.item())
     
-    metrics_dict={dataset:{'RMSE':(np.mean(RMSEs).numpy().round(decimals=3),np.std(RMSEs).numpy().round(decimals=3)),
-                           'LPP': (np.mean(LPPs).numpy().round(decimals=3),np.std(LPPs).numpy().round(decimals=3)),
-                           'PICP': np.mean(PICPs).numpy().round(decimals=3), 
-                           'MPIW': np.mean(MPIWs).numpy().round(decimals=3), 
+    metrics_dict={dataset:{'RMSE':(np.mean(RMSEs).round(decimals=3),np.std(RMSEs).round(decimals=3)),
+                           'LPP': (np.mean(LPPs).round(decimals=3),np.std(LPPs).round(decimals=3)),
+                           'PICP': np.mean(PICPs).round(decimals=3), 
+                           'MPIW': np.mean(MPIWs).round(decimals=3), 
                           }
                  }
     
